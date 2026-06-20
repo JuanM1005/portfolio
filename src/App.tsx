1784255b088b992fb4667afc148f031aa33c17c5
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2026-06-20 16:48:09 -0600

    refactor: quitar Componente container de prueba.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Container } from './components/ui';
-
 const App = () => {
   return (
     <main className="min-h-svh grid place-items-center bg-neutral-50 text-neutral-900">
-      <Container>
         <h1 className="text-3xl font-semibold tracking-tight">
           Portfolio - Setup listo
         </h1>
-      </Container>
     </main>
   );
 };
