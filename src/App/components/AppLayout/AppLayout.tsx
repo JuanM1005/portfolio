f85daf7e273744d808be6bc988d4a6490f6aa095
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2026-06-26 16:52:20 -0600

    feat(contact): renderiza sección y configura toaster.

--- src/App/components/AppLayout/AppLayout.tsx
+++ src/App/components/AppLayout/AppLayout.tsx
@@ -1,11 +1,12 @@
-import { Toaster } from 'react-hot-toast';
 import { Header, Footer } from '@/layouts';
 import { Hero, About, Skills, Projects, Roadmap, Education, Contact } from '@/sections';
+import { Toaster } from 'react-hot-toast';
 
 export const AppLayout = () => {
   return (
     <>
       <Header />
+
       <main id="home" className="min-h-svh">
         <Hero />
         <About />
@@ -15,8 +16,22 @@ export const AppLayout = () => {
         <Education />
         <Contact />
       </main>
+
       <Footer />
-      <Toaster position="bottom-center" />
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          duration: 3000,
+          className: 'md:max-w-2xl! border border-burgundy-600 bg-burgundy-100! p-2! text-xs font-medium text-burgundy-700!',
+
+          success: {
+            iconTheme: {
+              primary: 'var(--color-burgundy-600)',
+              secondary: 'var(--color-surface-card-muted)',
+            },
+          },
+        }}
+      />
     </>
   );
 };
