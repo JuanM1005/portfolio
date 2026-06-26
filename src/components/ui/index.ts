51c8c331cf67831b9aa23d1090047fba4b984ec4
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2026-06-26 16:55:04 -0600

    chore(ui): exporta nuevos componentes.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -7,3 +7,5 @@ export * from './Badge/Badge';
 export * from './Button/Button';
 export * from './Card/Card';
 export * from './CursorBlink/CursorBlink';
+export * from './Input/Input'
+export * from './Textarea/Textarea'
